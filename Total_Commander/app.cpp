e639fed639867bacd9c7e18260155d50613829dc
steve-warlock <89355017+steve-warlock@users.noreply.github.com>
2023-12-02 17:07:25 +0200

    Add files via upload

---
+++ Total_Commander/app.cpp
@@ -0,0 +1,23 @@
+#include "app.hpp"
+
+namespace app{
+
+Total_Commander::Total_Commander()
+{
+
+}
+
+Total_Commander::~Total_Commander()
+{
+
+}
+
+void Total_Commander::run(){}
+
+
+bool Total_Commander::closed()
+{
+ return false;
+}
+
+}
