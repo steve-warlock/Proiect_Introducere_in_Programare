7ea092522fc4500124cf43e34dab375b60ec177f
steve-warlock <89355017+steve-warlock@users.noreply.github.com>
2023-12-02 16:55:59 +0200

    Add files via upload

---
+++ Total_Commander/app.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+
+/// graphic and privilege libraries
+#include <graphics.h>
+#include <winbgim.h>
+#include <windows.h>
+#include <shellapi.h>
+
+
+///std
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <filesystem>
+#include <string>
+#include <stack>
+#include <queue>
+
+namespace app
+{
+
+class Total_Commander{
+  public:
+        Total_Commander();
+       ~Total_Commander();
+
+
+
+};
+
+}
+
