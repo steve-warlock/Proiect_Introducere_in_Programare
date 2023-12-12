1df756a95229c3789bb7f83e44fb4349a8b8ba0f
steve-warlock <89355017+steve-warlock@users.noreply.github.com>
2023-12-12 11:48:14 +0200

    Am facut sa apara path-ul, urmeaza de implementat listarea continutului unui fisier

--- main.cpp
+++ main.cpp
@@ -1,27 +1,71 @@
 #include <windows.h>
-#include <filesystem>
 #include <cstdlib>
 #include <fstream>
+#include <cstdio>
+#include <cstring>
+#include <string>
 #include <iostream>
 #include <winbgim.h>
 #include <graphics.h>
 #define Nr_of_Buttons 5
 using namespace std;
 
-
-string left_panel_path = "C:\\";
-string right_panel_path = "D:\\";
-
+char left_panel_path[FILENAME_MAX] = "C:\\Users\\Steve Warlock\\";
+char right_panel_path[FILENAME_MAX] = "D:\\";
 
 
-struct Files{
+
+struct node{
 
     string name;
     string extension;
     double size_file;
     string modified_date;
     bool is_directory;
+    node* next;
 };
+
+
+bool pathExists(const char* path) {
+    DWORD attributes = GetFileAttributes(path);
+    return (attributes != INVALID_FILE_ATTRIBUTES && (attributes & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+
+void display_path(int x1, int y1, int x2, int y2, const char* path) {
+    settextstyle(4, 0, 12);
+    setcolor(COLOR(151, 159, 165));
+    setbkcolor(COLOR(47, 61, 76));
+
+    if (pathExists(path)) {
+        const char* lastFolder = path + strlen(path) - 2;
+        while (lastFolder > path && *lastFolder != '\\') {
+            lastFolder--;
+        }
+
+        if (*lastFolder == '\\') {
+            lastFolder++; // Move past the '\\'
+        }
+
+        int textWidth = textwidth((char*)lastFolder);
+
+        if (textWidth > x2 - x1) {
+            int visibleLength = x2 - x1 - 4; // To leave space for ".."
+
+            if (visibleLength > 0) {
+                const char* truncatedPath = lastFolder + strlen(lastFolder) - visibleLength + 2;
+                char displayPath[FILENAME_MAX];
+                snprintf(displayPath, sizeof(displayPath), "..%s", truncatedPath);
+                outtextxy(x1 + 2, y1 + 7, displayPath);
+            }
+        } else {
+            outtextxy(x1 + 2, y1 + 7, (char*)path);
+        }
+    } else {
+        std::cout << "Path is not valid or doesn't exist\n";
+        // Handle accordingly, for example, displaying an error message
+    }
+}
 
 
 void DrawArrow(int x1, int y1, int x2, int y2, char* buttonText)
@@ -35,13 +79,14 @@ void DrawArrow(int x1, int y1, int x2, int y2, char* buttonText)
    outtextxy(textX, textY, buttonText);
 }
 
+
+
 void DrawShortcutButton(int x1, int y1, int x2, int y2, char* buttonText)
 {
    settextstyle(4,0,12);
    setcolor(COLOR(151,159,165));
    int textX = (x1 + x2) / 2 - textwidth(buttonText) / 2;
    int textY = (y1 + y2) / 2 - textheight(buttonText) / 2;
-   settextstyle(4,0,12);
    setbkcolor(COLOR(47,61,76));
    outtextxy(textX, textY, buttonText);
 }
@@ -58,20 +103,28 @@ void DrawButton(int x1, int y1, int x2, int y2, char* buttonText)
 }
 
 
-void free_space(string& current_path, int x1, int y1, int x2, int y2)
-{
+void free_space(const char* current_path, int x1, int y1, int x2, int y2) {
+    ULARGE_INTEGER freeBytesAvailable;
+    ULARGE_INTEGER totalNumberOfBytes;
+    ULARGE_INTEGER totalNumberOfFreeBytes;
 
-      try {
-        filesystem::space_info space = filesystem::space(current_path);
+    BOOL success = GetDiskFreeSpaceEx(
+        current_path,
+        &freeBytesAvailable,
+        &totalNumberOfBytes,
+        &totalNumberOfFreeBytes
+    );
 
-        std::string availableSpaceStr = std::to_string(space.available / (1024 * 1024 * 1024)) + " GB " + "free of " + std::to_string(space.capacity / (1024 * 1024 * 1024)) + " GB";
+    if (success) {
+        char availableSpaceStr[100];
+        snprintf(availableSpaceStr, sizeof(availableSpaceStr), "%llu GB free of %llu GB",
+                 (freeBytesAvailable.QuadPart) / (1024ULL * 1024ULL * 1024ULL),
+                 (totalNumberOfBytes.QuadPart) / (1024ULL * 1024ULL * 1024ULL));
 
         // Display text in the graphics window
-        DrawShortcutButton(x1, y1, x2, y2, (char*)availableSpaceStr.c_str());
-    } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "Filesystem error: " << e.what() << std::endl;
-    } catch (const std::exception& ex) {
-        std::cerr << "Error: " << ex.what() << std::endl;
+        DrawShortcutButton(x1, y1, x2, y2, availableSpaceStr);
+    } else {
+        std::cerr << "Error getting disk space information.\n";
     }
 }
 
@@ -94,7 +147,12 @@ void shortcut_buttons()
 
 void draw_first_panel()
 {
+    //sidebar de sus (display the memory)
     free_space(left_panel_path, 0, 0, 640, 30);
+
+    //display the path
+    display_path(0, 30, 640, 60, left_panel_path);
+
     //butoanele de deasupra fisierelor panel 1
     bar(270, 60, 273, 90);
     bar(355, 60, 358, 90);
@@ -112,16 +170,16 @@ void draw_first_panel()
     bar(355, 90, 358, 630);
     bar(440, 90, 443, 630);
 
-    //draw_arrow(615,60,640,90, UP);
-    //draw_arrow(615,630,640,660, DOWN);
-
-
-
 }
 
 void draw_second_panel()
 {
-        free_space(right_panel_path, 640, 0, 1280, 30);
+    //title bar de sus (display the memory)
+    //free_space(right_panel_path, 640, 0, 1280, 30);
+
+    //display the path
+   // display_path(x1,y1,x2,y2, right_panel_path);
+
     // butoanele de deasupra fisierelor panel 2
      bar(910, 60, 913, 90);
     bar(995, 60, 998, 90);
@@ -137,9 +195,6 @@ void draw_second_panel()
     bar(910, 90, 913, 630);
     bar(995, 90, 998, 630);
     bar(1080, 90, 1083, 630);
-
-     //draw_arrow(x1,y1,x2,y2, UP);
-    //draw_arrow(x1,y1,x2,y2, DOWN);
     DrawArrow(1264, 79, 1280, 90, (char*) "^");
     DrawArrow(1266, 647, 1280, 660, (char*) "v");
 }
